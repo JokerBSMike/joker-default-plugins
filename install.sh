22579dc93e899b7ed6b9a575cd16dac6b1a39e2c
JokerBSMike <145010212+JokerBSMike@users.noreply.github.com>
2026-05-07 16:19:11 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Navigate to the plugins folder
+cd plugins
+
+# Recursively unzip each zip folder inside to a folder of the same name
+find . -type f -name "*.zip" -exec wp plugin install {} \;
+
+#remove default plugins
+wp plugin uninstall akismet
+wp plugin uninstall hello
+
+#activate plugins
+wp plugin activate --all
+
+# Navigate to the themes folder
+cd ../themes
+
+# Recursively unzip each zip folder inside
+find . -type f -name "*.zip" -exec wp theme install {} \;
+
+#uninstall default themes leaving 1 fallback twentytwentyfive
+wp theme uninstall twentytwentythree
+wp theme uninstall twentytwentyfour
+
+#activate themes
+wp theme activate Divi
+
+#cleanup
+cd ..
+rm -rf repo
+
+
+
+
